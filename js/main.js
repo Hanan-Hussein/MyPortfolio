9dc92f5cc9c34888d9875f554e2e5968c8582e59
Hanan-Hussein <hanhuss035@gmail.com>
2022-02-27 20:54:04 +0300

    ft:Project functionality- redirecting functionality of a project

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 var linkedin= document.getElementById('linkedin');
 var mail = document.getElementById('mailing');
 var github=document.getElementById('github');
+var libraryProject = document.getElementById('library');
+var quiz = document.getElementById('quiz');
+var insurance = document.getElementById('insurance');
 
 
 // add eventlistener
 linkedin.addEventListener('click', linkedinEvent);
 mail.addEventListener('click', mailingEvent);
 github.addEventListener('click',githubEvent);
+libraryProject.addEventListener('click',libraryEvent);
+quiz.addEventListener('click',quizEvent);
+insurance.addEventListener('click',insuranceEvent);
 
 function linkedinEvent(e){
     e.preventDefault();
@@ -27,3 +33,25 @@ function githubEvent (e){
         '_blank'
     );
 }
+function libraryEvent(e){
+    e.preventDefault();
+    window.open(
+        'https://github.com/Hanan-Hussein/Online-Library-System',
+        '_blank'
+    );
+}
+function quizEvent(e){
+    e.preventDefault();
+    window.open(
+        'https://hanan-hussein.github.io/QuizBoard/',
+        '_blank'
+    );
+}
+function insuranceEvent (e){
+    e.preventDefault();
+    window.open(
+        'https://hanan-hussein.github.io/Insuarance-Website/',
+        '_blank'
+    );
+}
+
